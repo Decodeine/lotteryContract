5965f786cd5803ff5dbebd5cb561d9080abe6b45
root <root@DESKTOP-66U2FRP.localdomain>
2025-06-02 06:03:01 +0100

    chore: forge init

--- src/raffle.sol
+++ src/raffle.sol
@@ -1,18 +1,45 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.19;
+import {VRFConsumerBaseV2Plus} from "@chainlink/contracts/src/v0.8/vrf/dev/VRFConsumerBaseV2Plus.sol";
+import {VRFV2PlusClient} from "@chainlink/contracts/src/v0.8/vrf/dev/libraries/VRFV2PlusClient.sol";
+import {AutomationCompatibleInterface} from "@chainlink/contracts/src/v0.8/interfaces/AutomationCompatibleInterface.sol";
+
 /**
  * @title A simple raffle contract
  * @dev Inplement chainlink VRFv2.5  .
  * @author @Adesola
  * @notice This contract is for educational purposes only and should not be used in production.
  *  */
-contract Raffle{
+contract Raffle is VRFConsumerBaseV2Plus, AutomationCompatibleInterface{
     error Raffle__NotEnoughEth();
+    // Chainlink VRF Variables
+    uint256 private immutable i_subscriptionId;
+    bytes32 private immutable i_gasLane;
+    uint32 private immutable i_callbackGasLimit;
+    uint16 private constant REQUEST_CONFIRMATIONS = 3;
+    uint32 private constant NUM_WORDS = 1;
+
+    // Raffle Variables
     uint256 private immutable i_entranceFee;
     address payable[] private s_players;
     event raffleEntered(address indexed player);
-    constructor(uint256 entranceFee){i_entranceFee= entranceFee;}
-    
+     constructor(
+        uint256 subscriptionId,
+        bytes32 gasLane, // keyHash
+        uint256 interval,
+        uint256 entranceFee,
+        uint32 callbackGasLimit,
+        address vrfCoordinatorV2
+    ) VRFConsumerBaseV2Plus(vrfCoordinatorV2) {
+        i_gasLane = gasLane;
+        i_interval = interval;
+        i_subscriptionId = subscriptionId;
+        i_entranceFee = entranceFee;
+        s_raffleState = RaffleState.OPEN;
+        s_lastTimeStamp = block.timestamp;
+        i_callbackGasLimit = callbackGasLimit;
+
+    }
     
     function enterRaffle() public payable  {
         if(msg.value> i_entranceFee)
